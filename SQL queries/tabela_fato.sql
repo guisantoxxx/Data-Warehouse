cf233f148d6cc6fb86c0e949697e6627f9089cda
Guilherme Fernandes Rezende Santos <130177734+guisantoxxx@users.noreply.github.com>
2024-11-27 20:57:53 -0300

    Criação da tabela fato

---
+++ SQL queries/tabela_fato.sql
@@ -0,0 +1,11 @@
+-- Criação da tabela fato
+CREATE TABLE fato_vendas (
+	pk_produto INT NOT NULL,
+	pk_tempo INT NOT NULL,
+	pk_local INT NOT NULL,
+	valor REAL,
+	quantidade INT,
+	FOREIGN KEY(pk_produto) REFERENCES dimensao_produto(pk_produto)
+	FOREIGN KEY(pk_local) REFERENCES dimensao_local(pk_local)
+	FOREIGN KEY(pk_tempo) REFERENCES dimensao_tempo(pk_tempo)
+)

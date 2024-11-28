762e454420f3ab0d67f3302cab2d06dc121cbb29
Guilherme Fernandes Rezende Santos <130177734+guisantoxxx@users.noreply.github.com>
2024-11-27 21:05:24 -0300

    Add files via upload

---
+++ SQL queries/tabela_temp_dw.sql
@@ -0,0 +1,17 @@
+-- Criação e inserção dos valores na tabela temp			
+CREATE TABLE tabela_aux (
+	aux_id SERIAL PRIMARY KEY,
+	city VARCHAR,
+	product_name VARCHAR,
+	order_date date,
+	order_price REAL,
+	quantity BIGINT
+)
+
+
+INSERT INTO tabela_aux(city,product_name,order_date,order_price, quantity)
+SELECT cidade, nome, data_t, preco_total, qtd
+FROM dblink('dbname=Northwind ',
+                'SELECT  cidade, nome, data_t, preco_total, qtd FROM tabela_temp')
+				AS remote_table(cidade VARCHAR, nome VARCHAR, data_t DATE, preco_total REAL,
+							   qtd BIGINT)

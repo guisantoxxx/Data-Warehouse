451e0b7d378451e796aed2f1bab81a81bf792c0d
Guilherme Fernandes Rezende Santos <130177734+guisantoxxx@users.noreply.github.com>
2024-11-27 21:26:53 -0300

    Populando a tabela fato

--- SQL queries/populando_tabela_fato.sql
+++ SQL queries/populando_tabela_fato.sql
@@ -4,4 +4,4 @@ SELECT pk_produto, pk_tempo, pk_local, order_price, quantity
 FROM tabela_aux ta
 JOIN  dimensao_produto dp ON ta.product_name = dp.descricao
 JOIN dimensao_tempo dt ON ta.order_date = dt.datat
-JOIN dimensao_local dl ON ta.city = dl.cidade
+JOIN dimensao_local dl ON ta.city = dl.cidade;

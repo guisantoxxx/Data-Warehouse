e7f33515a08f119f7c2881adffbdf083af3397ad
Guilherme Fernandes Rezende Santos <130177734+guisantoxxx@users.noreply.github.com>
2024-11-27 21:04:01 -0300

    recuperação dos dados na tabela origem

---
+++ SQL queries/consultas_origem.sql
@@ -0,0 +1,21 @@
+-- Criando e populando a tabela temporária na origem para poder usar no DW
+CREATE TABLE tabela_temp (
+	temp_id SERIAL PRIMARY KEY,
+	cidade VARCHAR,
+	nome VARCHAR,
+	data_t DATE,
+	preco_total REAL,
+	qtd BIGINT
+)
+
+INSERT INTO tabela_temp(cidade, nome, data_t, preco_total, qtd)
+SELECT 
+    o.ship_city,
+    p.product_name, 
+    o.order_date, 
+    SUM(od.quantity * od.unit_price * (1 - od.discount)) AS total_price,
+    SUM(od.quantity) AS total_quantity
+FROM order_details od
+JOIN orders o ON od.order_id = o.order_id
+JOIN products p ON p.product_id = od.product_id
+GROUP BY p.product_name, o.order_date, o.ship_country, o.ship_city;

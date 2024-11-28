dc68e829143257443708c41cf7cf3da92be63dc6
Guilherme Fernandes Rezende Santos <130177734+guisantoxxx@users.noreply.github.com>
2024-11-27 21:06:44 -0300

    Passando todas as informações para a tabela fato

---
+++ SQL queries/populando_tabela_fato.sql
@@ -0,0 +1,7 @@
+-- Populando a tabela fato
+INSERT INTO fato_vendas(pk_produto, pk_tempo,pk_local, valor, quantidade)
+SELECT pk_produto, pk_tempo, pk_local, order_price, quantity
+FROM tabela_aux ta
+JOIN  dimensao_produto dp ON ta.product_name = dp.descricao
+JOIN dimensao_tempo dt ON ta.order_date = dt.datat
+JOIN dimensao_local dl ON ta.city = dl.cidade
